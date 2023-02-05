934285f40003f786b3ec5b6209183454eae98301
ranjabi <fikriranjabi02@gmail.com>
2023-02-05 23:03:13 +0700

    feat: add fragment file changes

---
+++ app/src/main/java/com/example/majika/view/CartFragment.kt
@@ -0,0 +1,31 @@
+package com.example.majika.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.majika.databinding.FragmentCartBinding
+
+class CartFragment : Fragment() {
+    private var _binding: FragmentCartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
