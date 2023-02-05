55fefdba40aedc1bb626d262d982c560c384fae7
ranjabi <fikriranjabi02@gmail.com>
2023-02-05 22:20:14 +0700

    chore: move mainActivity to view package

--- app/src/main/java/com/example/majika/view/MainActivity.kt
+++ app/src/main/java/com/example/majika/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.majika
+package com.example.majika.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.majika.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
