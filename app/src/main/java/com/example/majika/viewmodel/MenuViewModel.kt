50eb67a58f7b6cce429a8aa0337c9f95b004520f
Muhammad Fikri Ranjabi <13520002@std.stei.itb.ac.id>
2023-02-22 09:41:18 +0700

    fix: persist menu view model data in menu fragment

--- app/src/main/java/com/example/majika/viewmodel/MenuViewModel.kt
+++ app/src/main/java/com/example/majika/viewmodel/MenuViewModel.kt
@@ -1,30 +1,26 @@
 package com.example.majika.viewmodel
 
 import android.util.Log
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.majika.model.MenuItem
 import com.example.majika.network.MajikaApi
 import kotlinx.coroutines.launch
 
 private const val TAG = "MenuViewModel"
 
-class MenuViewModel : ViewModel() {
+class MenuViewModel(private val savedStateHandle: SavedStateHandle) : ViewModel() {
     private val _status = MutableLiveData<String>()
     val status: LiveData<String> = _status
 
-    private val _foodItem = MutableLiveData<List<MenuItem>>()
+    val _foodItem = MutableLiveData<List<MenuItem>>()
     val foodItem: LiveData<List<MenuItem>> = _foodItem
-    private val _drinkItem = MutableLiveData<List<MenuItem>>()
+    val _drinkItem = MutableLiveData<List<MenuItem>>()
     val drinkItem: LiveData<List<MenuItem>> = _drinkItem
 
     val _filteredFoodItem = MutableLiveData<List<MenuItem>>()
     val filteredFoodItem: LiveData<List<MenuItem>> = _filteredFoodItem
     val _filteredDrinkItem = MutableLiveData<List<MenuItem>>()
     val filteredDrinkItem: LiveData<List<MenuItem>> = _filteredDrinkItem
-
 
     init {
         getAllMenu()
